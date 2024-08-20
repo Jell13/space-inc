8cb69861fdeedfb8bff6fe8eec7b7ba1bdcabf71
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-20 16:34:03 -0700

    changed the 3d component for the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,34 +3,9 @@ import Spline from "@splinetool/react-spline";
 
 const page = () => {
   return (
-    // <div className='text-white md:px-44 px-12 w-screen h-full'>
-    //   <div className='w-full h-full flex flex-col gap-6 mt-10 mb-20 relative'>
-    //     <h3 className='font-bold text-2xl'>About Space.Inc</h3>
-        // <div className='flex flex-col gap-6'>
-        //   <p>The cosmos is more than just the final frontier. it's the ultimate playground for adventure
-        //     discovery, and inspiration. <br />
-        //     Bring wonders back to earth from the unknown. We provide a gateway to explore the vastness of the
-        //     universe. <br />
-        //     Journey through the unknown with us, where the only limit is your imagination.
-        //   </p>
-        //   <p>
-        //     Here in Space.Inc, we're passionate about turning the dream of space exploration into a reality for
-        //     everyone. <br />
-        //     Inspiring people's curiosity, fuel the spirit of exploration, and make the mysteries of space accessible 
-        //     to all. <br />
-        //     Space.Inc is your partner in the journey to explore the cosmos. Join us as we embark on adventures beyond 
-        //     the Earth and into the unknown.
-        //   </p>
-        // </div>
-    //     <div className='pb-20'>
-    //       <Spline scene="https://prod.spline.design/6gL6dzNIx3dKVwNV/scene.splinecode"/>
-    //     </div>
-    //        
-    //   </div>
-    // </div>
-    <div className='flex flex-col md:px-44 px-12'>
-      <div className='text-white'>
-        <p>Testing</p>
+    <div className='flex flex-col md:px-44 px-12 gap-4 mt-10 pb-20'>
+      <div className='text-white flex flex-col gap-4'>
+        <h3 className='font-bold text-2xl'>About Space.Inc</h3>
         <div className='flex flex-col gap-6'>
           <p>The cosmos is more than just the final frontier. it's the ultimate playground for adventure
             discovery, and inspiration. <br />
@@ -48,9 +23,8 @@ const page = () => {
           </p>
         </div>
       </div>
-      <Spline
-            className='z-0'
-            scene="https://prod.spline.design/6gL6dzNIx3dKVwNV/scene.splinecode" 
+          <Spline
+            scene="https://prod.spline.design/KvnNQ-SIi62Fi1jy/scene.splinecode" 
           />
     </div>
   )
---
+++ app/exploration/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='md:px-44 px-12 text-white mt-10'>
+      <h1>Exploration page</h1>
+    </div>
+  )
+}
+
+export default page
