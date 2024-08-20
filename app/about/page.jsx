df017b4df70ebbcc7b25f247e404dd869e13db3a
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-20 11:33:01 -0700

    give more effects on the mobile navigation and add more to the about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-white md:px-44 w-screen h-full'>
+    <div className='text-white md:px-44 px-12 w-screen h-full'>
       <div className='w-full h-full flex flex-col gap-6 mt-10'>
         <h3 className='font-bold text-2xl'>About Space.Inc</h3>
-        <div className='flex flex-col'>
-          <p>Venture throughout the final frontier</p>
+        <div className='flex flex-col gap-6'>
+          <p>The cosmos is more than just the final frontier. it's the ultimate playground for adventure
+            discovery, and inspiration. <br />
+            Bring wonders back to earth from the unknown. We provide a gateway to explore the vastness of the
+            universe. <br />
+            Journey through the unknown with us, where the only limit is your imagination.
+          </p>
+          <p>
+            Here in Space.Inc, we're passionate about turning the dream of space exploration into a reality for
+            everyone
+          </p>
         </div>
       </div>
     </div>
--- app/components/MobileNav.jsx
+++ app/components/MobileNav.jsx
@@ -41,7 +41,10 @@ const MobileNav = () => {
             <nav className='flex flex-col justify-center items-center gap-8 mt-32'>
                 {links.map(link => (
                     <SheetClose key={link.id} asChild>
-                        <Link className='text-xl text-white' href={link.link}>{link.name}</Link>
+                        <Link className='text-xl text-white group relative' href={link.link}>
+                            {link.name}
+                            <span className={`absolute border-b-[1px] left-0 border-white ${router === link.link ? 'w-full' : 'w-0'} hover:w-full duration-300`}>&nbsp;</span>
+                        </Link>
                     </SheetClose>
                 ))}
             </nav>
