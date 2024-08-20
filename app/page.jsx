7072948238810da4ea0fc6532acb96ba3d7565f4
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-20 15:29:21 -0700

    changed the size of the spline component

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
   return (
     <div className="h-full w-full flex items-center justify-center">
-      <div className="h-full">
+      <div className="h-[500px]">
         <Spline scene="https://prod.spline.design/q8AJdlcc0iDWxXKk/scene.splinecode"/>
       </div>
     </div>
